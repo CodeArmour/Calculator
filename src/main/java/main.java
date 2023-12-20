3255f26887bd26704d3fe3bd6d4ab29ea73dc10f
omars <omarsahmoud02@gmail.com>
2023-12-20 22:47:01 +0100

    Arrange the project

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,17 @@
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.integration.MidPointIntegrator;
+import org.apache.commons.math3.analysis.integration.SimpsonIntegrator;
+import org.apache.commons.math3.analysis.integration.TrapezoidIntegrator;
+import org.apache.commons.math3.analysis.integration.UnivariateIntegrator;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        UnivariateFunction function = v -> v + 1;
+        UnivariateIntegrator integrator = new MidPointIntegrator();
+        double result = integrator.integrate(1000, function, 0, 5);
+
+        System.out.println("Numerical result of the integral: " + result);
+
     }
 }
